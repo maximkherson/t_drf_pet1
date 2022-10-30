4bb942142bff5478c262285c61cb3a530314d7e8
Максим Башкатов <maximkherson@gmail.com>
2022-10-30 14:37:04 +0200

    Was done serialization with Serializer class.

--- woman/serializers.py
+++ woman/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
-from woman.models import Woman
 
 
-class WomanSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Woman
-        fields = ('title', 'cat_id')
+class WomanSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    cat_id = serializers.IntegerField()
--- woman/views.py
+++ woman/views.py
@@ -1,17 +1,21 @@
-from django.forms import model_to_dict
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from woman.models import Woman
+from woman.serializers import WomanSerializer
 
 
 class WomanAPIView(APIView):
     def get(self, request):
-        return Response({'posts': Woman.objects.all().values()})
+        print('XXX', request.data)
+        return Response({'posts': WomanSerializer(Woman.objects.all(), many=True).data})
 
     def post(self, request):
+        WomanSerializer(data=request.data).is_valid(raise_exception=True)
+
         new_post = Woman.objects.create(
             title=request.data['title'],
             content=request.data['content'],
             cat_id=request.data['cat_id']
         )
-        return Response({'post': model_to_dict(new_post)})
+
+        return Response({'post': WomanSerializer(new_post).data})
