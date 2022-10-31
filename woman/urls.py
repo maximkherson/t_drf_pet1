1fc2dbb0ffc20454dc0cf10a809fd3dfe16f1792
Максим Башкатов <maximkherson@gmail.com>
2022-10-31 19:11:30 +0200

    Was implemented feature - show category of concrete woman. Use @action decorator.

--- woman/urls.py
+++ woman/urls.py
@@ -1,12 +1,6 @@
-# from django.urls import path
-# from woman.views import WomanModelViewSet
-#
-# urlpatterns = [
-#     path('', WomanModelViewSet.as_view({'get': 'list'}), name='women'),
-#     path('<int:pk>', WomanModelViewSet.as_view({'get': 'retrieve', 'put': 'update'}))
-# ]
 from rest_framework import routers
 from woman.views import WomanModelViewSet
 
 women_router = routers.SimpleRouter()
 women_router.register('', WomanModelViewSet)
+print(women_router.urls)
--- woman/views.py
+++ woman/views.py
@@ -1,8 +1,15 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from woman.models import Woman
+from woman.models import Woman, Category
 from woman.serializers import WomanSerializer
 
 
 class WomanModelViewSet(ModelViewSet):
     queryset = Woman.objects.all()
     serializer_class = WomanSerializer
+
+    @action(methods=['get'], detail=True)
+    def category(self, request, pk=None):
+        category = Category.objects.get(pk=pk)
+        return Response({'category': category.name})
