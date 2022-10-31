f6c281ebfaf3334ab1713884a2dbee6f9625798b
Максим Башкатов <maximkherson@gmail.com>
2022-10-31 15:00:28 +0200

    ModelSerializer & ListCreateAPIView.

--- woman/urls.py
+++ woman/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from woman.views import WomanAPIView
+from woman.views import WomanListCreateAPIView
 
 urlpatterns = [
-    path('', WomanAPIView.as_view(), name='women'),
-    path('<int:pk>', WomanAPIView.as_view(), name='women'),
+    path('', WomanListCreateAPIView.as_view(), name='women')
 ]
