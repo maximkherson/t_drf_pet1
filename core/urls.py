daa9755100c9c680f4d41bf591dd975cdb5ad9ca
Максим Башкатов <maximkherson@gmail.com>
2022-10-31 17:28:17 +0200

    Add Simple Router.

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from woman.urls import women_router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('women/', include('woman.urls'))
+    # path('women/', include('woman.urls'))
+    path('women/', include(women_router.urls))
 ]
--- woman/urls.py
+++ woman/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+# from django.urls import path
+# from woman.views import WomanModelViewSet
+#
+# urlpatterns = [
+#     path('', WomanModelViewSet.as_view({'get': 'list'}), name='women'),
+#     path('<int:pk>', WomanModelViewSet.as_view({'get': 'retrieve', 'put': 'update'}))
+# ]
+from rest_framework import routers
 from woman.views import WomanModelViewSet
 
-urlpatterns = [
-    path('', WomanModelViewSet.as_view({'get': 'list'}), name='women'),
-    path('<int:pk>', WomanModelViewSet.as_view({'get': 'retrieve', 'put': 'update'}))
-]
+women_router = routers.SimpleRouter()
+women_router.register('', WomanModelViewSet)
